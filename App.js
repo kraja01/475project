c792133fe8c5fd6c952969896ca55429632517f2
Krithik Raja <krithikraja0@gmail.com>
2023-04-18 15:27:35 -0500

    inital push

--- App.js
+++ App.js
@@ -1,20 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { useState } from 'react';
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Camera, CameraType } from 'expo-camera';
+import { useCallback, useEffect } from 'react';
+import { createWorker } from 'tesseract.js';
 
-export default function App() {
+function StartScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title="Go to Main" onPress={() => navigation.navigate('Extract')} />
+    </View>
+  );
+}
+
+function MainScreen({ navigation }) {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  // function that can be used to change camera being used.
+  // function toggleCameraType() {
+  //   setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera style={styles.camera} type={type}></Camera>
     </View>
+  );
+}
+
+function scanImage({ navigation }) {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("");
+
+  const worker = createWorker();
+
+  const convertImageToText = useCallback(async () => {
+    if(!selectedImage) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const { data } = await worker.recognize(selectedImage);
+    setTextResult(data.text);
+  }, [worker, selectedImage]);
+
+  useEffect(() => {
+    convertImageToText();
+  }, [selectedImage, convertImageToText])
+
+  const handleChangeImage = e => {
+    if(e.target.files[0]) {
+      setSelectedImage(e.target.files[0]);
+    } else {
+      setSelectedImage(null);
+      setTextResult("")
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Extractor</h1>
+      <div className="input-wrapper">
+        <label htmlFor="upload">Upload Image: </label>
+        <input type="file" id="upload" accept='image/*' onChange={handleChangeImage} />
+      </div>
+
+      <div className="result">
+        {selectedImage && (
+          <div className="box-image">
+            <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
+          </div>
+        )}
+        {textResult && (
+          <div className="box-p">
+            <p>{textResult}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+const S = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <S.Navigator>
+        <S.Screen name="Start" component={StartScreen} />
+        <S.Screen name="Extract" component={scanImage} />
+        <S.Screen name="Main" component={MainScreen} />
+        
+      </S.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  camera: {
+    flex: 1,
+  },
 });
+
+export default App;
